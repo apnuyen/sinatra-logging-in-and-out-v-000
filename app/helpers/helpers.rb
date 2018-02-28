e4a1a63a6460a44755adc50ad603146a39ca96df
An Nuyen <annuyen@gmail.com>
2018-02-28 02:41:52 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,9 +1,9 @@
 class Helpers
-  def self.current_user(session_hash)
+  def self.current_user(session)
     @user = User.find(session[:user_id])
   end
 
-  def self.is_logged_in?(session_hash)
-    !session[:user_id]
+  def self.is_logged_in?(session)
+    !!session[:user_id]
   end
 end
